cb58817290a07bcf43c673cfe74728241ab42887
MissLiner <miss.liner@yahoo.com>
2022-03-21 16:52:10 -0400

    Link Experience to Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,13 +1,20 @@
+import Experience from './Experience';
 import Home from './Home';
 import './index.css';
 
 function Canvas(props) {
   const renderCanvas = () => {
-    if(props.view === "Home") {
+    if(props.view === "home") {
       return(
         <Home />
       )
+    } 
+    else if(props.view === "experience") {
+      return(
+        <Experience />
+      )
     }
+    else if()
   }
   return(
     <div className="Canvas main">
---
+++ src/components/Experience.js
@@ -0,0 +1,8 @@
+import './index.css';
+
+function Experience() {
+  return(
+    <div>Resume here . . . </div>
+  )
+}
+export default Experience;
