4236cd2ace50bec048649371ec9598782a349cf0
MissLiner <miss.liner@yahoo.com>
2022-03-21 16:48:50 -0400

    Add Canvas component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import Navbar from './components/Navbar';
 import './index.css';
 
 function App() {
+  const [view, setView] = useState("home");
+  const handleClick = (e) => {
+    setView(e.target.textcontent);
+  }
   return (
     <div>
       <div className="App header">
         <h1>Caroline Stephenson</h1>
-        <Navbar />
+        <Navbar clickFunc={handleClick} />
       </div>
+      
     </div>
   );
 }
---
+++ src/components/Canvas.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+function Canvas(props) {
+  const renderCanvas = () => {
+    if(props.view === "Home") {
+      return(
+        <Home />
+      )
+    }
+  }
+  return(
+    <div className="Canvas main">
+    </div>
+  )
+}
