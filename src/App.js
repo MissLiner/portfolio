ec2dd57d8f3ec479a9e86d05a35c7964841bd15e
MissLiner <miss.liner@yahoo.com>
2022-03-23 09:53:50 -0400

    Refactor App to handle all view changes and btnLabels

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import Canvas from './components/Canvas';
 import Navbar from './components/Navbar';
-import { activateBtn } from './components/helperFuncs'
-import { useState } from "react";
+import { activateBtn } from './components/helperFuncs';
+import { btnLabels } from './components/text';
+import { useEffect, useState } from "react";
 import './index.css';
 
 function App() {
   const [view, setView] = useState("home");
-  const [ canvasView, setCanvasView ] = useState("home");
+  const [canvasView, setCanvasView] = useState("home");
+  const [canvasBtns, setCanvasBtns] = useState("");
+
 
   const handleClickMain = (e) => {
     setView(e.target.value);
     activateBtn(e);
   }
   const handleClickCanvas = (e) => {
-    updateBtnList();
     activateBtn(e);
     setCanvasView(e.target.value);
+    setCanvasBtns(btnLabels[e.target.value])
   }
+
   return (
     <div>
       <div className="App header">
         <h1>Caroline Stephenson</h1>
-        <Navbar clickFunc={handleClickMain} navClass="mainNav" />
+        <Navbar clickFunc={handleClickMain} 
+                navClass="mainNav" 
+                btnList={ btnLabels.main}/>
       </div>
-      <Canvas view={view} />
+      <Navbar clickFunc={handleClickCanvas} 
+              navclass="canvasNav"
+              btnList={canvasBtns} />
+      <Canvas mainView={view}
+              canvasView={canvasView} />
     </div>
   );
 }
