ce586ab30c345ab205d1f7e87fd7d0090e4936ad
MissLiner <miss.liner@yahoo.com>
2022-03-28 11:24:00 -0400

    Remove slider div from ImageArray

--- src/components/ImageArray.js
+++ src/components/ImageArray.js
@@ -4,14 +4,13 @@ import '../index.css';
 function ImageArray(props) {
   const currentImage = props.images[props.index];
   return(
-    <div className="slider" id="slider">
       <img 
         src={currentImage[0]} 
         alt={currentImage[1]} 
         className="slider-pic"
+        id="slider-pic"
         >
       </img>
-    </div>
   )
 }
 export default ImageArray;
