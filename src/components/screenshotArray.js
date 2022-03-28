7aeadafe960efd18053b7cd8cc963cfa6a02ac03
MissLiner <miss.liner@yahoo.com>
2022-03-28 12:08:17 -0400

    Finish adding screenshot files and refs

--- src/components/screenshotArray.js
+++ src/components/screenshotArray.js
@@ -1,13 +1,25 @@
 import FSRestImg from '../images/screenshots/screengallery/FS_restaurant.svg';
-import TNRestImg from '/Users/Caroline/myprojects/portfolio/src/images/screenshots/screengallery/TN_restaurant.svg';
-import FSBattleImg from '/Users/Caroline/myprojects/portfolio/src/images/screenshots/screengallery/FS_battleship.svg';
-import TNBattleImg from '/Users/Caroline/myprojects/portfolio/src/images/screenshots/screengallery/TN_battleship.svg';
-import FSCalcImg from '/Users/Caroline/myprojects/portfolio/src/images/screenshots/screengallery/FS_calculator.svg';
-import TNCalcImg from '/Users/Caroline/myprojects/portfolio/src/images/screenshots/screengallery/TN_calculator.svg';
+import TNRestImg from '../images/screenshots/screengallery/TN_restaurant.svg';
+import FSBattleImg from '../images/screenshots/screengallery/FS_battleship.svg';
+import TNBattleImg from '../images/screenshots/screengallery/TN_battleship.svg';
+import FSCalcImg from '../images/screenshots/screengallery/FS_calculator.svg';
+import TNCalcImg from '../images/screenshots/screengallery/TN_calculator.svg';
+import FSMemory1Img from '../images/screenshots/screengallery/FS_memory-welscome.svg';
+import FSMemory2Img from '../images/screenshots/screengallery/FS_memory.svg';
+import FSCando1Img from '../images/screenshots/screengallery/FS_candolist.svg';
+import FSCando2Img from '../images/screenshots/screengallery/FS_candolist-add.svg';
+import FSTictacImg from '../images/screenshots/screengallery/FS_tictactoe.svg';
+import FSLibraryImg from '../images/screenshots/screengallery/FS_library.svg';
 
 
 export const screenshotArray = [
   [ FSRestImg, "restaurant site screenshot", TNRestImg ],
   [ FSBattleImg, "battleship site screenshot", TNBattleImg ],
   [ FSCalcImg, "calculator site screenshot", TNCalcImg],
+  [ FSMemory1Img, "memorycard welcome screen site screenshot"],
+  [ FSMemory2Img, "memorycard game screen site screenshot"],
+  [ FSCando1Img, "can do list site screenshot"],
+  [ FSCando2Img, "can do list add task site screenshot"],
+  [ FSTictacImg, "tic-tac-toe site screenshot"],
+  [ FSLibraryImg, "libary site screenshot"],
 ]
