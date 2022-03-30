9baccf69f35032a7a9846557839d7e8abc2848ba
MissLiner <miss.liner@yahoo.com>
2022-03-30 14:24:22 -0400

    Move ProjectDetailTags css into separate file and update class naming

--- src/components/ProjectDetailTags.js
+++ src/components/ProjectDetailTags.js
@@ -1,18 +1,18 @@
 import '../index.css';
-import '../styles/ProjectDetail.css';
+import '../styles/ProjectDetailTags.css';
 
 function ProjectDetailTags(props) {
   const renderTags = () => {
     return(
       props.currentProject.focus.map(item => {
         return(
-          <li className="ProjectDetailTags focus-list-item">{item}</li>
+          <li className="ProjectDetailTags-list-item">{item}</li>
         )
       })
     )
   }
   return(
-    <ul className="ProjectDetailTags focus-list">
+    <ul className="ProjectDetailTags">
       {renderTags()}
     </ul>
   )
