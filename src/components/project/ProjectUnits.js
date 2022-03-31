41642f52137386c61bd0f28619f43238c4a7035f
MissLiner <miss.liner@yahoo.com>
2022-03-31 13:20:56 -0400

    Show DetailUnits

--- src/components/project/ProjectUnits.js
+++ src/components/project/ProjectUnits.js
@@ -5,15 +5,17 @@ import "../../index.css"
 function ProjectUnits(props) {
   const project = props.currentProject;
   const renderUnits = () => {
-    project.units.map((unit, i) => {
-      return(
-        <div className="ProjectUnits-unit" key={"unit-" + i}>
-          <h3 className="ProjectUnits-title">{unit.name}</h3>
-          <SVGImg currentImage={unit.image} />
-          <p className="ProjectUnits-text">{unit.text}</p>
-        </div>
-      )
-    })
+    return(
+      project.units.map((unit, i) => {
+        return(
+          <div className="ProjectUnits-unit" key={"unit-" + i}>
+            <h3 className="ProjectUnits-title">{unit.name}</h3>
+            <SVGImg currentImage={unit.image} />
+            <p className="ProjectUnits-text">{unit.text}</p>
+          </div>
+        )
+      })
+    )
   }
   return(
     <div className="ProjectUnits">
