2900c39ab752326f887f3a1335ebc68297861f4e
MissLiner <miss.liner@yahoo.com>
2022-04-05 11:27:42 -0400

    Fix up About and styling

--- src/components/canvas_views/About.js
+++ src/components/canvas_views/About.js
@@ -1,8 +1,10 @@
 import '../../index.css';
 import './About.css';
+import { screenContent } from "../shared/text"
 
-function About() {
-  <div className="Canvas About content">
-  <p className="Canvas">{screenContent.about[props.canvasView]}</p>
+function About(props) {
+  <div className="About">
+    <p className="About-text">{screenContent.about[props.canvasView]}</p>
   </div>
 }
+export default About;
