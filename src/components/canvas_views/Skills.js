fcbb73a33b77f7d913ba87774ba4e57e0a0adf8e
MissLiner <miss.liner@yahoo.com>
2022-04-05 14:08:08 -0400

    Generate skills list

--- src/components/canvas_views/Skills.js
+++ src/components/canvas_views/Skills.js
@@ -5,14 +5,27 @@ import { skillsArr } from "../shared/skillsArr";
 function Skills() {
   const renderSkills = () => {
     return(
-      <div className="Skills-table">
-        <h3 className="Skills-category">{skillsArr[0].category}</h3>
-      </div>
+      skillsArr[0].skills.map(skill => {
+        return(
+          <div className="Skills-skill-div">
+            <img  className="Skills-skill-icon" 
+                  src={skill.image} 
+                  alt={skill.name + " icon"}>
+            </img>
+            <div className="Skills-skill-name">
+              {skill.name}
+            </div>
+          </div>
+        )
+      })
     )
   }
   return(
     <div className="Skills colors2">
-      {renderSkills()}
+      <div className="Skills-table">
+        <h3 className="Skills-category">{skillsArr[0].category}</h3>
+        {renderSkills()}
+      </div>
     </div>
   )
 }
