f840f14cb59c3599b81cc29dbb23610a16267914
MissLiner <miss.liner@yahoo.com>
2022-04-05 10:22:08 -0400

    Create About component

---
+++ src/components/main/About.js
@@ -0,0 +1,8 @@
+import '../../index.css';
+import './About.css';
+
+function About() {
+  <div className="Canvas About content">
+  <p className="Canvas">{screenContent.about[props.canvasView]}</p>
+  </div>
+}
