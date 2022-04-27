f02e884e5ab7bcba93875f863616a6ecd7f240ed
MissLiner <miss.liner@yahoo.com>
2022-04-27 13:12:09 -0400

    Place skills into two rows

--- src/components/canvas_views/Skills.js
+++ src/components/canvas_views/Skills.js
@@ -3,29 +3,57 @@ import './Skills.css';
 import { skillsArr } from "../shared/skillsArr";
 
 function Skills() {
-  const renderSkills = () => {
+  const renderFirstSkills = () => {
     return(
       skillsArr[0].skills.map((skill, i) => {
-        return(
-          <div className={"Skills-div Skills-div-" + i} key={"Skills-div" + i}>
-            <img  className="Skills-icon" 
-                  src={skill.image} 
-                  alt={skill.name + " icon"}>
-            </img>
-            <div className="Skills-name">
-              {skill.name}
+        if(skill.area === "row1") {
+          return(
+            <div className={"Skills-div Skills-div-" + i} key={"Skills-div" + i}>
+              <img  className="Skills-icon" 
+                    src={skill.image} 
+                    alt={skill.name + " icon"}>
+              </img>
+              <li className="Skills-name">
+                {skill.name}
+              </li>
             </div>
-          </div>
-        )
+          )
+        } else {
+          return null;
+        }
+      })
+    )
+  }
+  const renderSecondSkills = () => {
+    return(
+      skillsArr[0].skills.map((skill, i) => {
+        if(skill.area === "row2") {
+          return(
+            <div className={"Skills-div Skills-div-" + i} key={"Skills-div" + i}>
+              <img  className="Skills-icon" 
+                    src={skill.image} 
+                    alt={skill.name + " icon"}>
+              </img>
+              <li className="Skills-name">
+                {skill.name}
+              </li>
+            </div>
+          )
+        } else {
+          return null;
+        }
       })
     )
   }
   return(
     <div className="Skills">
       <div className="Skills-table">
-        <h3 className="Skills-category">{skillsArr[0].category}</h3>
-        <div className="Skills-list">
-        {renderSkills()}
+        <h2 className="Skills-category">{skillsArr[0].category}</h2>
+        <div className="Skills-list row1">
+        {renderFirstSkills()}
+        </div>
+        <div className="Skills-list row2">
+        {renderSecondSkills()}
         </div>
       </div>
     </div>
