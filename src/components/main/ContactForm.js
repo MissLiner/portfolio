89798eacaef3e5bab2b4561d55eea7e884eec8d6
MissLiner <miss.liner@yahoo.com>
2022-04-28 13:11:44 -0400

    Send email on submit

--- src/components/main/ContactForm.js
+++ src/components/main/ContactForm.js
@@ -1,20 +1,16 @@
 import '../../index.css';
 import './ContactForm.css';
 import emailjs from '@emailjs/browser';
-import { React, useState } from 'react';
+import { React, useRef } from 'react';
 
 function ContactForm() {
-  const [form, setForm] = useState({
-    senderName: "",
-    senderEmail: "",
-    senderPhone: "",
-    message: "",
-  });
+
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm("service_p1cor7p", "template_u2dllbh", form, "yhaJaUkUkfWSou52P")
+    emailjs.sendForm("service_p1cor7p", "template_u2dllbh", form.current, "yhaJaUkUkfWSou52P")
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -22,28 +18,11 @@ function ContactForm() {
       });
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   send(
-  //     "service_p1cor7p",
-  //     "template_u2dllbh",
-  //     toSend,
-  //     "yhaJaUkUkfWSou52P",
-  //   )
-  //     .then((response) => {
-  //       console.log('SUCCESS!', response.status, response.text);
-  //     })
-  //     .catch((err) => {
-  //       console.log('FAILED...', err);
-  //     });
-  // };
-
-  const handleChange = (e) => {
-    setForm({...form, [e.target.name]: e.target.value });
-  };
-
   return(
-    <form className="ContactForm">
+    <form 
+      className="ContactForm" 
+      ref={form}      
+      onSubmit={sendEmail}>
       <h3 className="ContactForm-title">Contact</h3>
       <label 
         htmlFor="senderName"
@@ -56,42 +35,21 @@ function ContactForm() {
         id="senderName"
         name="senderName"
         value={form.senderName}
-        onChange={handleChange}
       />
-      <div className="ContactForm-contactfield"
-        >should I call or email you?
-        <div className="ContactForm-input-unit">
-          <label 
-            htmlFor="senderEmail"
-            className="ContactForm-label"
-            >email me:
-          </label>
-          <input
-            className="ContactForm-input"
-            type="email"
-            id="senderEmail"
-            name="senderEmail"
-            placeholder="you@abc.com"
-            value={form.senderEmail}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="ContactForm-input-unit">
-          <label 
-            htmlFor="senderPhone"
-            className="ContactForm-label"
-            >call me:
-          </label>
-          <input
-            className="ContactForm-input"
-            type="number"
-            id="senderPhone"
-            name="senderPhone"
-            placeholder="(xxx) xxx-xxxx"
-            value={form.senderPhone}
-            onChange={handleChange}
-          />
-        </div>
+      <div className="ContactForm-input-unit">
+        <label 
+          htmlFor="senderEmail"
+          className="ContactForm-label"
+          >email me:
+        </label>
+        <input
+          className="ContactForm-input"
+          type="email"
+          id="senderEmail"
+          name="senderEmail"
+          placeholder="you@abc.com"
+          value={form.senderEmail}
+        />
       </div>
       <div className="ContactForm-input-unit">
         <label 
@@ -105,13 +63,11 @@ function ContactForm() {
           id="message"
           name="message"
           value={form.message}
-          onChange={handleChange}
         />
       </div>
       <button
         className="ContactForm-submitbtn"
         type="submit"
-        onSubmit={sendEmail}
         >submit
       </button>
     </form>
