a719e667442b46438b3ea2b6e2c5141f2bfcd19e
MissLiner <miss.liner@yahoo.com>
2022-04-28 16:58:19 -0400

    Fix bug so filter isn't green

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -10,49 +10,62 @@ import './App.css';
 function App() {
   const [view, setView] = useState("home");
   const [showContact, setShowContact] = useState(false);
+  const [filter, setFilter] = useState("");
 
   const handleClickHome = () => {
     setView("home");
   }
   const handleClickEmail = () => {
     setShowContact(true);
+    setFilter("App-filter");
   }
   const handleClickNav = (e) => {
     setView(e.target.value);
   }
   const hideContact = () => {
     setShowContact(false);
+    setFilter("");
   }
 
   const renderContactForm = () => {
     if(showContact) {
       return(
-        <div className="App-filter">
         < ContactForm submitFunc={hideContact} />
-        </div>
       )
     } else {
       return null;
     }
   }
+  // const renderFilter = () => {
+  //   if(showContact) {
+  //     return(
+  //       <div className="App-filter"></div>
+  //     )
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
   return (
     <div className="App">
-      <StickyNav 
-        homeFunc={handleClickHome} 
-        emailFunc={handleClickEmail} 
-      />
-      <div className="App-header colors-primary">
-        <h1 className="App-name">Caroline Stephenson</h1>
-        <Navbar 
-          clickFunc={handleClickNav} 
-          btnList={navLabels}
-          clearView="home"
-          view={view}
-         />
-      </div>
-      <Canvas mainView={view} />
+      <div className={filter + " App-screen"}>
+        <StickyNav 
+          homeFunc={handleClickHome} 
+          emailFunc={handleClickEmail} 
+        />
+        <div className="App-header colors-primary">
+          <h1 className="App-name">Caroline Stephenson</h1>
+          <Navbar 
+            clickFunc={handleClickNav} 
+            btnList={navLabels}
+            clearView="home"
+            view={view}
+          />
+        </div>
+        <Canvas mainView={view} />
+        </div>
       {renderContactForm()}
+      {/* {renderFilter()} */}
     </div>
   );
 }
